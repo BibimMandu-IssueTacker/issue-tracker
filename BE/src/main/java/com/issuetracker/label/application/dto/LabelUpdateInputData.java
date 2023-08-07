21ebc7faf8e0b2270b07dafe2b16b3a3e71b41be
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2023-08-08 04:12:13 +0900

    feat/#88: 라벨 수정 API 추가

--- BE/src/main/java/com/issuetracker/label/application/dto/LabelUpdateInputData.java
+++ BE/src/main/java/com/issuetracker/label/application/dto/LabelUpdateInputData.java
@@ -5,20 +5,23 @@ import com.issuetracker.label.domain.Label;
 import lombok.Getter;
 
 @Getter
-public class LabelCreateInputData {
+public class LabelUpdateInputData {
 
+	private Long id;
 	private String title;
 	private String description;
 	private String color;
 
-	public LabelCreateInputData(String title, String description, String color) {
+	public LabelUpdateInputData(Long id, String title, String description, String color) {
+		this.id = id;
 		this.title = title;
 		this.description = description;
 		this.color = color;
 	}
 
-	public Label toLabelForCreate() {
+	public Label toLabelForUpdate() {
 		return Label.builder()
+			.id(id)
 			.title(title)
 			.description(description)
 			.color(color)
